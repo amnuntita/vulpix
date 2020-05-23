64dfa00d5351a79f41d50dc01ea29e48d3f0473c
Am <amnuntita19@gmail.com>
2020-05-23 16:00:34 +0700

    env and gitignore

---
+++ build/precache-manifest.4beffa0f46012af56bceeaa11bab5b44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70dfe404e08b1c94ef6aa7bfbbc72944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "642992fd04511315b62c",
+    "url": "/static/css/2.08bd5f8d.chunk.css"
+  },
+  {
+    "revision": "f14ad43fa45061f287e3",
+    "url": "/static/css/main.731b872b.chunk.css"
+  },
+  {
+    "revision": "642992fd04511315b62c",
+    "url": "/static/js/2.a9bd8861.chunk.js"
+  },
+  {
+    "revision": "096e326a1ca6418fea5e6c0ac344431d",
+    "url": "/static/js/2.a9bd8861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14ad43fa45061f287e3",
+    "url": "/static/js/main.f8976413.chunk.js"
+  },
+  {
+    "revision": "e73745b7420a587138d7",
+    "url": "/static/js/runtime-main.f92b6cf5.js"
+  }
+]);
