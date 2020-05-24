661a27b7bb9fc0c5c419b70369ac48654fc88664
Am <amnuntita19@gmail.com>
2020-05-24 13:17:01 +0700

    adjust url

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get("/res/", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/public", Express.static("build"));
+  app.use(Express.static("build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join("build", "index.html"));
   });
--- src/shared/BaseUrl.js
+++ src/shared/BaseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "https://vulpix-cucp.herokuapp.com/public";
+export const baseUrl = "https://vulpix-cucp.herokuapp.com";
