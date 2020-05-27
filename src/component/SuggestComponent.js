42038d74f51a3f58be924a576a16f2ca88863ce3
Am <amnuntita19@gmail.com>
2020-05-27 16:04:42 +0700

    return from map

--- src/component/SuggestComponent.js
+++ src/component/SuggestComponent.js
@@ -3,16 +3,18 @@ import { ListGroupItem } from "reactstrap";
 import { Link } from "react-router-dom";
 
 const SuggestComponent = ({ suggestItems }) => {
-  console.log(suggestItems);
+  console.log(suggestItems[0].title);
   return (
     <div style={{ fontSize: "small" }}>
       {suggestItems && suggestItems.length !== 0 ? (
-        suggestItems.map((app) => (
-          //Link is better as it only move root page and makes it routable
-          <Link key={app.title} to={`/result/${app.title}`}>
-            <ListGroupItem>{app.title}</ListGroupItem>
-          </Link>
-        ))
+        suggestItems.map((app) => {
+          return (
+            //Link is better as it only move root page and makes it routable
+            <Link key={app.title} to={`/result/${app.title}`}>
+              <ListGroupItem>{app.title}</ListGroupItem>
+            </Link>
+          );
+        })
       ) : (
         // If There no search result, show No Result!
         <ListGroupItem>-----------</ListGroupItem>
