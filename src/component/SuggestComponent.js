aa7a97b4019329d66aea0a20d6a477a21657f7e0
Am <amnuntita19@gmail.com>
2020-05-27 16:59:15 +0700

    remove condition in suggest component

--- src/component/SuggestComponent.js
+++ src/component/SuggestComponent.js
@@ -6,19 +6,14 @@ const SuggestComponent = ({ suggestItems }) => {
   console.log(suggestItems);
   return (
     <div style={{ fontSize: "small" }}>
-      {suggestItems ? (
-        suggestItems.map((app) => {
-          return (
-            //Link is better as it only move root page and makes it routable
-            <Link key={app.title} to={`/result/${app.title}`}>
-              <ListGroupItem>{app.title}</ListGroupItem>
-            </Link>
-          );
-        })
-      ) : (
-        // If There no search result, show No Result!
-        <ListGroupItem>-----------</ListGroupItem>
-      )}
+      {suggestItems.map((app) => {
+        return (
+          //Link is better as it only move root page and makes it routable
+          <Link key={app.title} to={`/result/${app.title}`}>
+            <ListGroupItem>{app.title}</ListGroupItem>
+          </Link>
+        );
+      })}
     </div>
   );
 };
